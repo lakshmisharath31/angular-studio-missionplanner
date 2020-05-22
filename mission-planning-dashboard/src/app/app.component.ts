b7ae112a8520179524a0e0dd888c478f45eadd99
Lakshmi Sharath <lakshmi@lakshmis-air.home>
2020-05-21 21:10:16 -0400

    first commit

---
+++ mission-planning-dashboard/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Mission-planning-dashboard';
+  name='Mars 2030'
+  Rocket_name='Plasma Max'
+}
